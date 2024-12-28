fec9b4f7ed483f6f080a329f7a6c2da321b63066
Luna Levi <itnguyenvantoan@gmail.com>
2024-12-28 23:55:31 +0700

    dnd trello column part 1

--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,18 +1,52 @@
+import {useState, useEffect} from 'react'
 import Box from '@mui/material/Box'
 import ListColumns from './ListColumns/ListColumns'
 import { mapOrder } from '~/utils/sorts'
+// dnd-kit
+import { DndContext } from '@dnd-kit/core'
 
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  const [orderedColumns, setOrderedColumns] = useState([])
+  
+  useEffect(() => {
+    const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+    setOrderedColumns(orderedColumns)
+  }, [board])
+
+  const handleDragEnd = (event) => {
+    console.log('handleDragEnd', event)
+    const { active, over } = event
+    
+    if (active.id !== over.id) {
+      // Lấy ra index của column đang kéo
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id)
+      // Lấy ra index của column đang kéo qua
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id)
+      // Clone ra mảng mới
+      const newColumns = [...orderedColumns]
+      // Thêm column đang kéo vào vị trí mới
+      newColumns.splice(oldIndex, 1)
+      newColumns.splice(newIndex, 0, orderedColumns[oldIndex])
+      // Update lại columnOrderIds
+      // const newColumnOrderIds = newColumns.map(c => c._id)
+      // Gọi API update lại columnOrderIds
+      // ...
+      // Update lại state
+      setOrderedColumns(newColumns)
+    }
+  }
+
   return (
-    <Box sx={{
-      bgcolor: (theme) => theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
-      width: '100%',
-      height: (theme) => theme.trello.boardContentHeight,
-      p: '10px 0'
-    }}>
-      <ListColumns columns={ orderedColumns } />
-    </Box>
+    <DndContext onDragEnd={handleDragEnd}>
+      <Box sx={{
+        bgcolor: (theme) => theme.palette.mode === 'dark' ? '#34495e' : '#1976d2',
+        width: '100%',
+        height: (theme) => theme.trello.boardContentHeight,
+        p: '10px 0'
+      }}>
+        <ListColumns columns={ orderedColumns } />
+      </Box>
+    </DndContext>
   )
 }
 
--- src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/Column.jsx
@@ -17,10 +17,26 @@ import AddCardIcon from '@mui/icons-material/AddCard'
 import DragHandleIcon from '@mui/icons-material/DragHandle'
 import Button from '@mui/material/Button'
 import ListCards from './ListCards/ListCards'
+// utils sort
 import { mapOrder } from '~/utils/sorts'
+// dnd-kit
+import {useSortable} from '@dnd-kit/sortable'
+import {CSS} from '@dnd-kit/utilities'
 
 
 function Column({ column }) {
+  const { attributes, listeners, setNodeRef, transform, transition, } = useSortable({
+    id: column._id,
+    data: {...column },
+  });
+
+  const dndKitColumnStyles = {
+    // Nếu sử dụng css.transform như docs sẽ lỗi kiểu stretch
+    // https://github.com/clauderic/dnd-kit/issues/117
+    transform: CSS.Translate.toString(transform),
+    transition,
+  };
+
   const [anchorEl, setAnchorEl] = React.useState(null)
   const open = Boolean(anchorEl)
   const handleClick = (event) => setAnchorEl(event.currentTarget)
@@ -29,7 +45,12 @@ function Column({ column }) {
   const orderedCards = mapOrder(column?.cards, column?.cardOrderIds, '_id')
 
   return (
-    <Box sx={{
+    <Box
+      ref={setNodeRef}
+      style={dndKitColumnStyles}
+      {...attributes}
+      {...listeners}
+      sx={{
       minWidth: '300px',
       maxWidth: '300px',
       bgcolor: (theme) => theme.palette.mode === 'dark' ? '#333643' : '#ebecf0',
